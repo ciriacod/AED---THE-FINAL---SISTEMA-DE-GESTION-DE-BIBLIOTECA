f7594e23760eb622ec4b817bfdd354171e7e8138
AlejandroAyme <ale100student@gmail.com>
2026-06-24 21:55:25 -0500

    Initialize App class with data structures and scanner

--- QuickLibrary/src/App.java
+++ QuickLibrary/src/App.java
@@ -1,5 +1,10 @@
+import java.util.Scanner;
+import estructuras.LinkedBST;
+import estructuras.LinkedQueue;
+import modelos.Libro;
+import modelos.Solicitud;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        
-    }
-}
+    private static LinkedBST<Libro> bstLibros = new LinkedBST<>();
+    private static LinkedQueue<Solicitud> colaSolicitudes = new LinkedQueue<>();
+    private static Scanner scanner = new Scanner(System.String.getScanner() != null ? System.getScanner() : new Scanner(System.in));
