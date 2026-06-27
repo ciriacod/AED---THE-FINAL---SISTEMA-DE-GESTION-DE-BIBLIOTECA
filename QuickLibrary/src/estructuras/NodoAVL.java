0e67ea42ae25ba73e2d0162e7dd46569688dd002
Joshua8801 <juan.hincho@estudiante.ucsm.edu.pe>
2026-06-27 13:32:55 -0500

    set y get de dato

--- QuickLibrary/src/estructuras/NodoAVL.java
+++ QuickLibrary/src/estructuras/NodoAVL.java
@@ -3,6 +3,26 @@ package estructuras;
 public class NodoAVL<E> {
 
     private E dato;
+    private NodoAVL<E> izquierdo;
+    private NodoAVL<E> derecho;
+    private int altura;
+
+    public NodoAVL(E dato) {
+        this.dato = dato;
+        this.izquierdo = null;
+        this.derecho = null;
+        this.altura = 1;
+    }
+
+    public E getDato() {
+        return dato;
+    }
+
+    public void setDato(E dato) {
+        this.dato = dato;
+    }
+
+
 
 
 }
