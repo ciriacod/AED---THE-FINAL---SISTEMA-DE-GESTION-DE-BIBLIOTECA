d3e9d0dd23cd9e7f2281ce763128715e58128e43
Joshua8801 <juan.hincho@estudiante.ucsm.edu.pe>
2026-06-27 15:57:04 -0500

    raiz atributo, constructor y get

--- QuickLibrary/src/estructuras/ArbolAVL.java
+++ QuickLibrary/src/estructuras/ArbolAVL.java
@@ -1,6 +1,16 @@
 package estructuras;
 
 public class ArbolAVL<E extends Comparable<E>>{
+    private NodoAVL<E> raiz;
+    
+    public ArbolAVL() {
+        raiz = null;
+    }
 
+    
+    public NodoAVL<E> getRaiz() {
+        return raiz;
+    }
 
 }
+
