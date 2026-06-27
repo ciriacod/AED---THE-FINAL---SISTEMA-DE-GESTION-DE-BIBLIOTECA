8d6bb0a4caa17dc8a1ed1a103b14d3d216d97c0f
Joshua8801 <juan.hincho@estudiante.ucsm.edu.pe>
2026-06-27 16:01:17 -0500

    El metodo insertar del ArbolAVL

--- QuickLibrary/src/estructuras/ArbolAVL.java
+++ QuickLibrary/src/estructuras/ArbolAVL.java
@@ -7,6 +7,37 @@ public class ArbolAVL<E extends Comparable<E>>{
         raiz = null;
     }
 
+    //Inserta un elemento en el árbol
+    
+    public void insertar(E dato) {
+        raiz = insertar(raiz, dato);
+    }
+
+    private NodoAVL<E> insertar(NodoAVL<E> nodo, E dato) {
+
+        if (nodo == null)
+            return new NodoAVL<>(dato);
+
+        if (dato.compareTo(nodo.getDato()) < 0) {
+
+            nodo.setIzquierdo(
+                    insertar(nodo.getIzquierdo(), dato));
+
+        } else if (dato.compareTo(nodo.getDato()) > 0) {
+
+            nodo.setDerecho(
+                    insertar(nodo.getDerecho(), dato));
+
+        } else {
+
+            return nodo;
+        }
+
+        actualizarAltura(nodo);
+
+        return balancear(nodo);
+    }
+
     
     public NodoAVL<E> getRaiz() {
         return raiz;
