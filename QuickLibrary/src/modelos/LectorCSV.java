6578d7893470f7a6140eb824a75c580df09030a1
ciriacod <caico2009yepes@hotmail.com>
2026-06-28 00:21:36 -0500

    Refactor library management system: - Updated LectorCSV class to include CSV reading functionality and a main method for testing. - Removed GestionBiblioteca and LinkedBST classes as part of code cleanup. - Added new GestorBiblioteca class with unresolved dependencies. - Introduced ILibroControlador, IPrestamoControlador, and IReporteControlador interfaces with unresolved package issues. - Implemented ArbolAVL and NodoAVL classes for tree structure management. - Updated test.csv to fix encoding issues.

--- QuickLibrary/src/modelos/LectorCSV.java
+++ QuickLibrary/src/modelos/LectorCSV.java
@@ -1,11 +1,34 @@
 package modelos;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Path;
+import java.nio.file.Files;
+
 public class LectorCSV {
     private String ruta;
 
-    LectorCSV(String ruta){
+    public LectorCSV(String ruta){
         this.ruta = ruta;
     }
 
-    // temporal
+    public void leer() {
+        try(BufferedReader br = Files.newBufferedReader(Path.of(ruta), 
+        StandardCharsets.UTF_8)){
+            String linea;
+
+            while ((linea = br.readLine()) != null) {
+                System.out.println(linea);
+                
+            }
+        } catch (IOException e){
+            e.printStackTrace();
+        }
+
+    }
+
+    public static void main(String[] args) {
+        new LectorCSV("test.csv").leer();
+    }
 }
