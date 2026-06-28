302f241b13e3d2803cda1ead394d2f7997626c46
try-69 <alexis.subia.luna@gmail.com>
2026-06-27 19:23:14 -0500

    Update GestorBiblioteca.java

--- QuickLibrary/src/controladores/gestor/GestorBiblioteca.java
+++ QuickLibrary/src/controladores/gestor/GestorBiblioteca.java
@@ -1,1 +1,22 @@
+package controlador.impl;
 
+import controlador.interfaces.ILibroControlador;
+import controlador.interfaces.IPrestamoControlador;
+import controlador.interfaces.IReporteControlador;
+import estructuras.ArbolBinarioBusqueda;  // Hay q cambiar el nombre segun como coloquen el arbol
+import estructuras.ColaGenerica;   // Hay q cambiar el nombre segun como coloquen la cola
+import modelos.Libro;
+import modelos.SolicitudPrestamo;
+
+public class GestorBiblioteca implements ILibroControlador, IPrestamoControlador, IReporteControlador {
+    
+    // Dependemos de las abstracciones de las estructuras
+    private final ArbolBinarioBusqueda<Libro> catalogoLibros;
+    private final ColaGenerica<SolicitudPrestamo> colaSolicitudes;
+
+    // Constructor que recibe las estructuras
+    public GestorBiblioteca(ArbolBinarioBusqueda<Libro> catalogoLibros, ColaGenerica<SolicitudPrestamo> colaSolicitudes) {
+        this.catalogoLibros = catalogoLibros;
+        this.colaSolicitudes = colaSolicitudes;
+    }
+}
