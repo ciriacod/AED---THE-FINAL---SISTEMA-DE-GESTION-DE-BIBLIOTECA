ba45105ac794eb55e9389bace1406c5a25058ebf
ciriacod <caico2009yepes@hotmail.com>
2026-06-28 19:07:53 -0500

    Refactor App class to initialize MenuPrincipal and remove obsolete code; add LectorCSV class methods for loading books and update test data format

--- QuickLibrary/src/modelos/LectorCSV.java
+++ QuickLibrary/src/modelos/LectorCSV.java
@@ -1,9 +1,13 @@
 package modelos;
 
+import java.io.BufferedInputStream;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Path;
+
+import estructuras.LinkedQueue;
+
 import java.nio.file.Files;
 
 public class LectorCSV {
@@ -13,19 +17,16 @@ public class LectorCSV {
         this.ruta = ruta;
     }
 
-    public void leer() {
-        try(BufferedReader br = Files.newBufferedReader(Path.of(ruta), 
-        StandardCharsets.UTF_8)){
-            String linea;
+    public LinkedQueue<Libro> cargarLibros(){
+        LinkedQueue<Libro> libreriaCargada = new LinkedQueue<>();
 
-            while ((linea = br.readLine()) != null) {
-                System.out.println(linea);
-                
-            }
-        } catch (IOException e){
+        try (BufferedReader br = ){
+            
+        } catch (IOException e) {
             e.printStackTrace();
         }
 
+        return libreriaCargada;
     }
 
     public static void main(String[] args) {
