2e6995a35737f070a52df337d5bc6c2a24bb4ff6
try-69 <alexis.subia.luna@gmail.com>
2026-06-29 14:47:34 -0500

    Update ILibroControlador.java

--- QuickLibrary/src/controladores/interfaces/ILibroControlador.java
+++ QuickLibrary/src/controladores/interfaces/ILibroControlador.java
@@ -1,8 +1,14 @@
 package controladores.interfaces;
 
 import modelos.Libro;
+// impotacion de una lista
 
 public interface ILibroControlador {
     void registrarLibro(Libro libro);
+    void mostrarTodosLosLibros();
+    void modificarLibro(int codigo, String nuevoTitulo, String nuevoAutor, String nuevaCategoria, int nuevoAnio);
+    void eliminarLibro(int codigo);
+    void mostrarLibrosPorEstado(String estado);  // Muestra cuando el libro este prestado o disponible
     Libro buscarLibroPorCodigo(int codigo);
+    void buscarLibrosPorCriterio(String criterio, String valor);  // Busqueda del libro ya sea por titulo, autor o categoria
 }
